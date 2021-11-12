85da804f5d2474b05a96a6974a614cc73eef41a5
jayapal <jayapal.d@gmail.com>
2021-11-13 01:48:39 +0530

    add validation of form

--- js/style.js
+++ js/style.js
@@ -2,28 +2,53 @@ let namefield = document.getElementById("namefield");
 let username = document.getElementById("username");
 let mbno = document.getElementById("mbno");
 let email = document.getElementById("email");
-let password= document.getElementById("password");
+let password = document.getElementById("password");
 let confirmpassword = document.getElementById("confirmpassword");
 let error = document.getElementById("error");
+let password_text = document.getElementById("password_text");
 
-// console.log(/^([0-9]{9})$/.test("0123456789"));
-console.log("style.js")
-function validate(){
+console.log((/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[#$%@*!`~^&+=])(?=\S+$).{8,}$/).test('123s#RANYA@'));
+console.log("style.js");
+let numbercase = /^[0-9]+$/
+let lowercase = /^([a-z]+)$/;
+let uppercase = /^[A-Z]+$/;
+
+
+function validate() {
     console.log("inside");
-
-    // let mbno_regex = /^([0-9]{10})$/
     let mbno_regex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-    if (mbno_regex.test(mbno.value)){
-        console.log(mbno_regex.test(mbno));
+    let password_regex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[#$%@*!`~^&+=])(?=\S+$).{8,}$/;
+
+    if (mbno_regex.test(mbno.value) && password.value == confirmpassword.value && password_regex.test(password.value)) {
         error.innerHTML = "valid";
-        error.style.color = "green";       
+        error.style.color = "green";
         return true;
     }
-    else{
-        console.log(mbno_regex.test(mbno.value));
-        error.innerHTML = "invalid ph no";
-        rror.style.color = "red";
+
+    else if (password_regex.test(password.value) == false) {
+        password.style.background = "red";
         return false;
     }
-    
+
+    else if (mbno_regex.test(mbno.value) == false) {
+        mbno.style.background = "red";
+        return false;
+    }
+
+}
+
+
+function trigger() {
+    if(numbercase.test(password.value) || lowercase.test(password.value) || uppercase.test(password.value)){
+        console.log(trigger)
+        password.style.backgroundColor = "red";
+    }
+    else if(numbercase.test(password.value) && lowercase.test(password.value)){
+        password_text.style.color = "green";
+    }
+   
+   
+
+
+
 }
